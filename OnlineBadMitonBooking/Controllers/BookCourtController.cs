a9aebdcfcbaf330472bdb0ddc0c486e59b6b0e1c
dhivyajothi88 <dhivyajothi88@gmail.com>
2020-02-12 18:45:25 +0530

    Add project files.

---
+++ OnlineBadMitonBooking/Controllers/BookCourtController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineBadMitonBooking.Controllers
+{
+    public class BookCourtController : Controller
+    {
+        // GET: BookCourt
+        public  string Booking()
+        {
+            return "Booked successfully";
+        }
+    }
+}
---
+++ OnlineBadMitonBooking/Controllers/CancelBookCourtController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineBadMitonBooking.Controllers
+{
+    public class CancelBookCourtController : Controller
+    {
+        // GET: CancelBookCourt
+        public string CancelBooking()
+        {
+            return "The Booking is cancelled";
+        }
+    }
+}
